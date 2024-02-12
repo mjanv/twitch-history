ed1f792ce178905199d7949db21ffcd24c511478
Maxime Janvier <maxime.janvier@gmail.com>
2024-02-12 23:50:36 +0100

    lol

--- lib/twitch_story/apis/twitch_api.ex
+++ lib/twitch_story/apis/twitch_api.ex
@@ -38,6 +38,39 @@ defmodule TwitchStory.Apis.TwitchApi do
       {:ok, %Req.Response{status: 200, body: %{"data" => data}}} -> data
       _ -> []
     end
-    |> Enum.map(fn emote -> Map.take(emote, ["name"]) end)
+
+    # |> Enum.map(fn emote -> Map.take(emote, ["name"]) end)
   end
+
+  def channel(broadcaster_id) do
+    Req.new(base_url: "https://api.twitch.tv")
+    |> Req.Request.put_headers([
+      {"Authorization", "Bearer #{token()}"},
+      {"Client-Id", @api[:client_id]}
+    ])
+    |> Req.get(url: "/helix/channels?broadcaster_id=#{broadcaster_id}")
+    |> case do
+      {:ok, %Req.Response{status: 200, body: %{"data" => data}}} -> data
+      error -> error
+    end
+  end
+
+  def user(broadcaster_id) do
+    Req.new(base_url: "https://api.twitch.tv")
+    |> Req.Request.put_headers([
+      {"Authorization", "Bearer #{token()}"},
+      {"Client-Id", @api[:client_id]}
+    ])
+    |> Req.get(url: "/helix/users?id=#{broadcaster_id}")
+    |> case do
+      {:ok, %Req.Response{status: 200, body: %{"data" => data}}} -> data
+      error -> error
+    end
+  end
+
+  """
+  %{"emote_set_id" => "327382231", "emote_type" => "subscriptions", "format" => ["static", "animated"], "id" => "emotesv2_7800b6632ca846979bf47440bbb26a5d", "images" => %{"url_1x" => "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_7800b6632ca846979bf47440bbb26a5d/static/light/1.0", "url_2x" => "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_7800b6632ca846979bf47440bbb26a5d/static/light/2.0", "url_4x" => "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_7800b6632ca846979bf47440bbb26a5d/static/light/3.0"}, "name" => "flonflLenie", "scale" => ["1.0", "2.0", "3.0"], "theme_mode" => ["light", "dark"], "tier" => "1000"}
+  %{"broadcaster_id" => "468884133", "broadcaster_language" => "fr", "broadcaster_login" => "flonflon", "broadcaster_name" => "Flonflon", "content_classification_labels" => [], "delay" => 0, "game_id" => "509658", "game_name" => "Just Chatting", "is_branded_content" => false, "tags" => ["Français", "musique", "playlist"], "title" => "LA PLAYLIST IDÉALE... DES ONE-HIT WONDERS (les artistes qui n'ont fait qu'un tube) 🎶 | !soutenir !instagram !holy"}
+  %{"broadcaster_type" => "partner", "description" => "Just Chatting", "display_name" => "flonfl", "email" => "fol", "id" => "468884133", "login" => "flonfl", "offline_image_url" => "https://static-cdn.jtvnw.net/jtv_user_pictures/3e3e3e3e-3e3e-3e3e-3e3e-3e3e3e3e3e3e-channel_offline_image-1920x1080.png", "profile_image_url" => "https://static-cdn.jtvnw.net/jtv_user_pictures/3e3e3e3e-3e3e-3e3e-3e3e-3e3e3e3e3e3e-profile_image-300x300.png", "type" => "", "view_count" => 0}
+  """
 end
--- test/twitch_story/apis/twitch_api_test.exs
+++ test/twitch_story/apis/twitch_api_test.exs
@@ -44,4 +44,38 @@ defmodule TwitchStory.Apis.TwitchApiTest do
              %{"name" => "flonflIncroyable"}
            ]
   end
+
+  test "channel/1" do
+    assert TwitchApi.channel(468_884_133) == %{
+             "broadcaster_id" => "468884133",
+             "broadcaster_name" => "flonfl",
+             "broadcaster_language" => "fr",
+             "game_id" => "509670",
+             "game_name" => "Just Chatting",
+             "title" => "Just Chatting",
+             "is_mature" => false,
+             "started_at" => "2021-07-01T18:00:00Z",
+             "thumbnail_url" =>
+               "https://static-cdn.jtvnw.net/previews-ttv/live_user_flonfl-{width}x{height}.jpg",
+             "tag_ids" => ["6ea6bca4-4712-4ab9-a906-e3336a9d8039"],
+             "is_tag_rouge" => true
+           }
+  end
+
+  test "user/1" do
+    assert TwitchApi.user(468_884_133) == %{
+             "id" => "468884133",
+             "login" => "flonfl",
+             "display_name" => "flonfl",
+             "type" => "",
+             "broadcaster_type" => "partner",
+             "description" => "Just Chatting",
+             "profile_image_url" =>
+               "https://static-cdn.jtvnw.net/jtv_user_pictures/3e3e3e3e-3e3e-3e3e-3e3e-3e3e3e3e3e3e-profile_image-300x300.png",
+             "offline_image_url" =>
+               "https://static-cdn.jtvnw.net/jtv_user_pictures/3e3e3e3e-3e3e-3e3e-3e3e-3e3e3e3e3e3e-channel_offline_image-1920x1080.png",
+             "view_count" => 0,
+             "email" => "fol"
+           }
+  end
 end
